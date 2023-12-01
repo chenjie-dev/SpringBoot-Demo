b8cc4406076180a8bbf0144a07b077acefdce793
chenjie <chenjie@hongwuniu.com>
2023-12-01 17:54:40 +0800

    2023年12月1日更新

--- spring-boot-elasticsearch-test/src/main/java/com/chenjie/elasticsearch/ESDemoApplication.java
+++ spring-boot-elasticsearch-test/src/main/java/com/chenjie/elasticsearch/ESDemoApplication.java
@@ -1,7 +1,5 @@
 package com.chenjie.elasticsearch;
 
-
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,7 +8,6 @@ import java.util.TimeZone;
 
 
 @SpringBootApplication
-@MapperScan(value = "com.chenjie.elasticsearch")
 @EnableScheduling
 public class ESDemoApplication {
     public static void main(String[] args) {
--- spring-boot-kafka-test/src/main/java/com/chenjie/kafka/KafkaTest.java
+++ spring-boot-kafka-test/src/main/java/com/chenjie/kafka/KafkaTest.java
@@ -1,12 +1,14 @@
 package com.chenjie.kafka;
 
-import com.alibaba.fastjson.JSONObject;
 import com.chenjie.kafka.service.KafkaService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 
 @RunWith(SpringRunner.class)
@@ -18,7 +20,14 @@ public class KafkaTest {
 
     private static final String testTopic = "chenjie-test-topic";
     @Test
-    public void test() {
-        kafkaService.send(testTopic, "test"+System.currentTimeMillis());
+    public void test() throws InterruptedException, ExecutionException {
+        while (true) {
+            Future result = kafkaService.send(testTopic, "test"+System.currentTimeMillis());
+            System.out.println("result.get() ----> " + result.get());
+            System.out.println("result.isDone() ----> " + result.isDone());
+            System.out.println("睡眠一秒");
+            System.out.println();
+            Thread.sleep(1000);
+        }
     }
 }
